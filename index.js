1d541d2cdeae5430d9bb0cbccbb78eb847a63553
odongo <saonarawestinc@outlook.com>
2024-03-23 05:33:54 +0300

    Completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31,
+  num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100);
+
+const num3 = 19,
+  num4 = 5;
+let mod = num3 % num4;
+
+const arr = [1, 20, 3, 4, 5];
+let max = Math.max(...arr);
